211a87260c56000aec2eb96f2c96d1069ecbb8a4
manupanangad <134674580+manupanangad@users.noreply.github.com>
2026-01-02 19:55:23 +1100

    Add project files.

---
+++ Eshoppe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eshoppe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
