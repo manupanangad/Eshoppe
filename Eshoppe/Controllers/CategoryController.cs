eb5f705565aa4f7ee103e5388fe65a4b5e6dde33
manupanangad <134674580+manupanangad@users.noreply.github.com>
2026-01-05 16:11:18 +1100

    Create category option added.

--- Eshoppe/Controllers/CategoryController.cs
+++ Eshoppe/Controllers/CategoryController.cs
@@ -16,5 +16,23 @@ namespace Eshoppe.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); 
+            }
+
+            return View();
+        }
     }
 }
--- Eshoppe/Models/Category.cs
+++ Eshoppe/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Eshoppe.Models
 {
@@ -7,9 +8,11 @@ namespace Eshoppe.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Name is required")]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
 
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
