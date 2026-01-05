50e54dc7cbe3e3f84d06b4f3465700d478a12ca6
manupanangad <134674580+manupanangad@users.noreply.github.com>
2026-01-05 11:53:56 +1100

    Added Entity framework reference for creating database.

---
+++ Eshoppe/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Eshoppe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eshoppe.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
