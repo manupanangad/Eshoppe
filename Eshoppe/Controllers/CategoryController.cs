51adf5e3a42e376aaff7730ac4c51be2531bc638
manupanangad <134674580+manupanangad@users.noreply.github.com>
2026-01-07 14:03:30 +1100

    Repository Pattern implemented

--- Eshoppe/Controllers/CategoryController.cs
+++ Eshoppe/Controllers/CategoryController.cs
@@ -1,19 +1,21 @@
 ﻿using Eshoppe.DataAccess;
 using Eshoppe.Models;
+using EShoppe.DataAccess.Repository;
+using EShoppe.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eshoppe.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryController(ICategoryRepository categoryRepository)
         {
-            _db = db;
+            _categoryRepository = categoryRepository;
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _categoryRepository.GetAll().ToList();
             return View(objCategoryList);
         }
 
@@ -31,8 +33,8 @@ namespace Eshoppe.Controllers
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(category);
-                _db.SaveChanges();
+                _categoryRepository.Add(category);
+                _categoryRepository.Save();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
@@ -46,7 +48,7 @@ namespace Eshoppe.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb = _categoryRepository.Get(u => u.Id == id);
             if (categoryFromDb == null)
             {
                 return NotFound();
@@ -59,8 +61,8 @@ namespace Eshoppe.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(category);
-                _db.SaveChanges();
+                _categoryRepository.Update(category);
+                _categoryRepository.Save();
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
@@ -73,7 +75,7 @@ namespace Eshoppe.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb = _categoryRepository.Get(u => u.Id == id);
             if (categoryFromDb == null)
             {
                 return NotFound();
@@ -81,22 +83,22 @@ namespace Eshoppe.Controllers
             return View(categoryFromDb);
         }
 
-        [HttpPost,ActionName("Delete")]
+        [HttpPost, ActionName("Delete")]
         public IActionResult DeletePost(int? id)
         {
             if (id == 0 || id == null)
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb = _categoryRepository.Get(u => u.Id == id);
             if (categoryFromDb == null)
             {
                 return NotFound();
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Remove(categoryFromDb);
-                _db.SaveChanges();
+                _categoryRepository.Remove(categoryFromDb);
+                _categoryRepository.Save();
                 TempData["success"] = "Category deleted successfully";
                 return RedirectToAction("Index");
             }
--- Eshoppe/Program.cs
+++ Eshoppe/Program.cs
@@ -1,10 +1,13 @@
 using Eshoppe.DataAccess;
+using EShoppe.DataAccess.Repository;
+using EShoppe.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
